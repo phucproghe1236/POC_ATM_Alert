904c0d057bf06f72ead91adb1012a0827f52ded5
CongKien <74692561+kckien16@users.noreply.github.com>
2021-08-30 21:45:29 +0700

    commit

---
+++ demo.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const demo = () => {
+    return (
+        <View>
+            <Text>Hello</Text>
+        </View>
+    )
+}
+
+export default demo
+
+const styles = StyleSheet.create({})
